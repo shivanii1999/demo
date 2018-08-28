43da8aceb8ec14576a9a75b12a996174078e57fa
Dell <Dell@Admin>
2018-08-28 19:21:15 +0530

    hey

---
+++ JavaGit/src/githubdemo/demo.java
@@ -0,0 +1,11 @@
+package githubdemo;
+
+public class demo {
+
+	public static void main (String[] args) {
+		System.out.println("Hello git!");
+		System.out.println("Hello eclipse");
+		System.out.println("Hello github");
+	}
+	
+}
